4dfc2a36da7536b5110ce5e8a2050a4706a9920f
17mital <121709091+17mital@users.noreply.github.com>
2024-12-26 12:08:40 +0530

    Update SectionTwo.jsx

--- src/components/About/SectionTwo.jsx
+++ src/components/About/SectionTwo.jsx
@@ -30,7 +30,7 @@ const SectionTwo = () => {
             <span>Product Design, Photography, Web Design </span>&{" "}
             <span>Graphic Design.</span>
           </h3> */}
-          <p style={{color:"Brown"}} className="odio aos-init aos-animate" data-aos="fade-up">
+          <p style={{color:"#664343"}} className="odio aos-init aos-animate" data-aos="fade-up">
           Take pride in my achievements, including earning a 5-Star Gold Badge in C++ on HackerRank and building impactful projects like WanderLust (an Airbnb clone) and a Trip & Daily Expense Tracker mobile app. Currently, I am enhancing my problem-solving skills by delving into Data Structures and Algorithms (DSA) using Java.In the future, I aim to deepen my expertise in machine learning, seeking to merge these skills with web development to create innovative solutions.Beyond academics and technology, I am an artist at heart, sharing my sketches, paintings, and doodles through my YouTube channel and Instagram art account. These platforms reflect my creativity, discipline, and attention to detail—traits I bring to every project and collaboration.
           </p>
         </div>
